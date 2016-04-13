c64b03fad39194848f9d7212952251ddd0da0286
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-04-13 16:44:32 +0200

    SeanceController

--- app/Http/Controllers/SeanceController.php
+++ app/Http/Controllers/SeanceController.php
@@ -2,85 +2,331 @@
 
 namespace App\Http\Controllers;
 
+use App\Seance;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class SeanceController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @SWG\Get(
+     *     path="/seance",
+     *     summary="Display a listing of seances.",
+     *     tags={"seance"},
+     *     @SWG\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @SWG\Schema(
+     *              type="array",
+     *              @SWG\Items(ref="#/definitions/Seance")
+     *          ),
+     *     ),
+     *  )
      */
     public function index()
     {
-        //
+        $seances = Seance::all();
+        return $seances;
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @SWG\Post(
+     *     path="/seance",
+     *     summary="Create a seance",
+     *     description="Use this method to create a seance",
+     *     operationId="createSeance",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"seance"},
+     *     @SWG\Parameter(
+     *         description="ID of the movie",
+     *         in="formData",
+     *         name="id_film",
+     *         required=true,
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the room",
+     *         in="formData",
+     *         name="id_salle",
+     *         required=true,
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the opener person",
+     *         in="formData",
+     *         name="id_personne_ouvreur",
+     *         required=true,
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the tech person",
+     *         in="formData",
+     *         name="id_personne_technicien",
+     *         required=true,
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the cleaning person",
+     *         in="formData",
+     *         name="id_personne_menage",
+     *         required=true,
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Starting time of the seance",
+     *         in="formData",
+     *         name="debut_seance",
+     *         required=true,
+     *         type="string",
+     *         maximum="255" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Ending time of the seance",
+     *         in="formData",
+     *         name="fin_seance",
+     *         required=true,
+     *         type="string",
+     *         maximum="255" 
+     *     ),
+     *     @SWG\Response(
+     *         response=201,
+     *         description="Seance created",
+     *         @SWG\Schema(
+     *              ref="#/definitions/Seance",
+     *         ),
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Missing required fields",
+     *     )
+     * )
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'id_film' => 'required|numeric|exists:films',
+            'id_salle' => 'required|numeric|exists:salles',
+            'id_personne_ouvreur' => 'required|numeric|exists:personnes',
+            'id_personne_technicien' => 'required|numeric|exists:personnes',
+            'id_personne_menage' => 'required|numeric|exists:personnes',
+            'debut_seance' => 'required|max:255',
+            'fin_seance' => 'required|max:255'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(
+                ['errors' => $validator->errors()->all()],
+                422);
+        }
+
+        $seance = new Seance;
+        $seance->id_film = $request->id_film;
+        $seance->id_salle = $request->id_salle;
+        $seance->id_personne_ouvreur = $request->id_personne_ouvreur;
+        $seance->id_personne_technicien = $request->id_personne_technicien;
+        $seance->id_personne_menage = $request->id_personne_menage;
+        $seance->debut_seance = $request->debut_seance;
+        $seance->fin_seance = $request->fin_seance;
+        $seance->save();
+        
+        return response()->json(
+            $seance,
+            201);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @SWG\Get(
+     *      path="/seance/{id_seance}",
+     *      summary="Display a single seance",
+     *      description="Use this method to return a single seance attributes based on its id.",
+     *      operationId="showSeance",
+     *      tags={"seance"},
+     *      @SWG\Parameter(
+     *          name="id_seance",
+     *          in="path", 
+     *          type="integer",
+     *          description="id of seance to fetch",
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *           @SWG\Schema(
+     *               ref="#/definitions/Seance",
+     *           ),
+     *      ),
+     *      @SWG\Response(
+     *           response=404, 
+     *           description="Seance not found"
+     *       ),
+     * )
      */
     public function show($id)
     {
-        //
+        $seance = Seance::find($id);
+
+        if(empty($seance)){
+            return response()->json(
+                ['error' => 'This seance does not exist'],
+                404);
+        }
+
+        return $seance;
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @SWG\Put(
+     *     path="/seance/{id_seance}",
+     *     summary="Update a seance",
+     *     description="Use this method to update the attributes of a seance based on its id.",
+     *     operationId="updateSeance",
+     *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
+     *     tags={"seance"},
+     *     @SWG\Parameter(
+     *         description="ID of the seance",
+     *         in="path",
+     *         name="id_seance",
+     *         type="integer"
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the movie",
+     *         in="formData",
+     *         name="id_film",
+     *         type="integer"
+     *     ), 
+     *     @SWG\Parameter(
+     *         description="ID of the room",
+     *         in="formData",
+     *         name="id_salle",
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the opener person",
+     *         in="formData",
+     *         name="id_personne_ouvreur",
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the tech person",
+     *         in="formData",
+     *         name="id_personne_technicien",
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="ID of the cleaning person",
+     *         in="formData",
+     *         name="id_personne_menage",
+     *         type="integer" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Starting time of the seance",
+     *         in="formData",
+     *         name="debut_seance",
+     *         type="string",
+     *         maximum="255" 
+     *     ),
+     *     @SWG\Parameter(
+     *         description="Ending time of the seance",
+     *         in="formData",
+     *         name="fin_seance",
+     *         type="string",
+     *         maximum="255" 
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Seance updated",
+     *         @SWG\Schema(
+     *              ref="#/definitions/Seance",
+     *         ),
+     *     ),
+     *     @SWG\Response(
+     *         response=422,
+     *         description="Champs manquants obligatoires ou incorrects"
+     *     ),
+     * )
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'id_film' => 'numeric|exists:films',
+            'id_salle' => 'numeric|exists:salles',
+            'id_personne_ouvreur' => 'numeric|exists:personnes',
+            'id_personne_technicien' => 'numeric|exists:personnes',
+            'id_personne_menage' => 'numeric|exists:personnes',
+            'debut_seance' => 'max:255',
+            'fin_seance' => 'max:255'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(
+                ['errors' => $validator->errors()->all()],
+                422);
+        }
+
+        $seance = Seance::find($id);
+        $seance->id_film = $request->id_film != null ? $request->id_film : $seance->id_film;
+        $seance->id_salle = $request->id_salle != null ? $request->id_salle : $seance->id_salle;
+        $seance->id_personne_ouvreur = $request->id_personne_ouvreur != null ? $request->id_personne_ouvreur : $seance->id_personne_ouvreur;
+        $seance->id_personne_technicien = $request->id_personne_technicien != null ? $request->id_personne_technicien : $seance->id_personne_technicien;
+        $seance->id_personne_menage = $request->id_personne_menage != null ? $request->id_personne_menage : $seance->id_personne_menage;
+        $seance->debut_seance = $request->debut_seance != null ? $request->debut_seance : $seance->debut_seance;
+        $seance->fin_seance = $request->fin_seance != null ? $request->fin_seance : $seance->fin_seance;
+        $seance->save();
+
+        return response()->json(
+            $seance,
+            200);
     }
 
-    /**
-     * Remove the specified resource from storage.
+     /**
+     * @SWG\Delete(
+     *     path="/seance/{id_seance}",
+     *     summary="Delete a seance",
+     *     description="Use this method to delete a seance based on its id.",
+     *     operationId="deleteSeance",
+     *     tags={"seance"},
+     *     @SWG\Parameter(
+     *         description="Seance ID to delete",
+     *         in="path",
+     *         name="id_seance",
+     *         required=true,
+     *         type="integer",
+     *         format="int64"
+     *     ),
+     *     @SWG\Response(
+     *         response=200,
+     *         description="Seance deleted"
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Invalid seance value"
+     *     )
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * )
      */
     public function destroy($id)
     {
-        //
+        $user = JWTAuth::parseToken()->authenticate();
+        if ($user->type != 1){
+            return response()->json(
+                ['error' => 'You don\'t have authorization for this content'],
+                403);
+        } else {
+            $film = Seance::find($id);
+
+            if (empty($seance)) {
+                return response()->json(
+                    ['error' => 'this seance does not exist'],
+                    404);
+            }
+
+            $seance->delete();
+        }
+    }
     }
 }
