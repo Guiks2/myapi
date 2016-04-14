405b7c9f665e77c360040d3ade4eb84a44c9f638
ThibaultCrosnier <thibault.crosnier@gmail.com>
2016-04-14 14:46:56 +0200

    Correction du DistributeurController

--- app/Http/Controllers/DistributeurController.php
+++ app/Http/Controllers/DistributeurController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class DistributeurController extends Controller
 {
@@ -14,10 +15,11 @@ class DistributeurController extends Controller
      * @SWG\Get(
      *     path="/distributeur",
      *     summary="Display a listing of distributors.",
+     *     operationId="indexDistributeur",
      *     tags={"distributeur"},
      *     @SWG\Response(
      *          response=200,
-     *          description="successful operation",
+     *          description="Successful operation",
      *          @SWG\Schema(
      *              type="array",
      *              @SWG\Items(ref="#/definitions/Distributeur")
@@ -44,11 +46,11 @@ class DistributeurController extends Controller
      *     path="/distributeur",
      *     summary="Create a distributor",
      *     description="Use this method to create a distributor",
-     *     operationId="createDistributor",
+     *     operationId="storeDistributeur",
      *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
      *     tags={"distributeur"},
      *     @SWG\Parameter(
-     *         description="Nom du distributeur",
+     *         description="Name of the distributor",
      *         in="formData",
      *         name="nom",
      *         required=true,
@@ -56,39 +58,35 @@ class DistributeurController extends Controller
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Adresse du distributeur",
+     *         description="Address of the distributor",
      *         in="formData",
      *         name="adresse",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Code postal du distributeur",
+     *         description="Zipcode of the distributor",
      *         in="formData",
      *         name="cpostal",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Ville du distributeur",
+     *         description="City of the distributor",
      *         in="formData",
      *         name="ville",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Pays du distributeur",
+     *         description="Country of the distributor",
      *         in="formData",
      *         name="pays",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Téléphone du distributeur",
+     *         description="Phone number of the distributor",
      *         in="formData",
      *         name="telephone",
      *         required=true,
@@ -104,7 +102,7 @@ class DistributeurController extends Controller
      *     ),
      *     @SWG\Response(
      *         response=422,
-     *         description="Champs manquant obligatoire ou incorrect"
+     *         description="Missing or incorrect fields"
      *     )
      * )
      */
@@ -112,7 +110,11 @@ class DistributeurController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'nom' => 'required|unique:distributeurs|max:255',
-            'telephone' => 'required|max:255'
+            'telephone' => 'required|max:30',
+            'adresse' => 'max:255',
+            'cpostal' => 'numeric',
+            'ville' => 'max:255',
+            'pays' => 'max:255'
         ]);
 
         if($validator->fails()){
@@ -140,13 +142,14 @@ class DistributeurController extends Controller
      *      path="/distributeur/{id_distributeur}",
      *      summary="Display a single distributor",
      *      description="Use this method to return a single distributor attributes based on its id.",
-     *      operationId="showDistributor",
+     *      operationId="showDistributeur",
      *      tags={"distributeur"},
      *      @SWG\Parameter(
      *          name="id_distributeur",
      *          in="path", 
      *          type="integer",
-     *          description="id of distributeur to fetch",
+     *          required=true,
+     *          description="Distributor ID",
      *      ),
      *      @SWG\Response(
      *          response=200,
@@ -167,7 +170,7 @@ class DistributeurController extends Controller
 
         if(empty($distributeur)){
             return response()->json(
-                ['error' => 'This distributeur does not exist'],
+                ['error' => 'Distributor not found'],
                 404);
         }
         return $distributeur;
@@ -178,11 +181,11 @@ class DistributeurController extends Controller
      *     path="/distributeur/{id_distributeur}",
      *     summary="Update a distributor",
      *     description="Use this method to update the attributes of a distributor based on its id.",
-     *     operationId="updateDistributor",
+     *     operationId="updateDistributeur",
      *     consumes={"multipart/form-data", "application/x-www-form-urlencoded"},
      *     tags={"distributeur"},
      *     @SWG\Parameter(
-     *         description="ID distributor",
+     *         description="Distributor ID",
      *         in="path",
      *         name="id_distributeur",
      *         required=true,
@@ -190,50 +193,44 @@ class DistributeurController extends Controller
      *         maximum="11"
      *     ),
      *     @SWG\Parameter(
-     *         description="Nom du distributeur",
+     *         description="Name of the distributor",
      *         in="formData",
      *         name="nom",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Adresse du distributeur",
+     *         description="Address of the distributor",
      *         in="formData",
      *         name="adresse",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Code postal du distributeur",
+     *         description="Zipcode of the distributor",
      *         in="formData",
      *         name="cpostal",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Ville du distributeur",
+     *         description="City of the distributor",
      *         in="formData",
      *         name="ville",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Pays du distributeur",
+     *         description="Country of the distributor",
      *         in="formData",
      *         name="pays",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
      *     @SWG\Parameter(
-     *         description="Téléphone du distributeur",
+     *         description="Phone number of the distributor",
      *         in="formData",
      *         name="telephone",
-     *         required=false,
      *         type="string",
      *         maximum="255"
      *     ),
@@ -246,15 +243,23 @@ class DistributeurController extends Controller
      *     ),
      *     @SWG\Response(
      *         response=422,
-     *         description="Champs manquants obligatoires ou incorrects"
+     *         description="Missing or incorrect fields"
+     *     ),
+     *     @SWG\Response(
+     *         response=404,
+     *         description="Distributor not found"
      *     ),
      * )
      */
     public function update(Request $request, $id)
     {
         $validator = Validator::make($request->all(), [
-            'nom' => 'max:255',
-            'telephone' => 'max:255'
+            'nom' => 'unique:distributeurs|max:255',
+            'telephone' => 'max:30',
+            'adresse' => 'max:255',
+            'cpostal' => 'numeric',
+            'ville' => 'max:255',
+            'pays' => 'max:255'
         ]);
 
         if($validator->fails()){
@@ -264,18 +269,19 @@ class DistributeurController extends Controller
         }
 
         $distributeur = Distributeur::find($id);
+
         if(empty($distributeur)){
             return response()->json(
-                ['error' => 'Movie not found'],
+                ['error' => 'Distributor not found'],
                 404);
         }
 
-        $distributeur->nom = $request->nom;
-        $distributeur->adresse = $request->adresse;
-        $distributeur->cpostal = $request->cpostal;
-        $distributeur->ville = $request->ville;
-        $distributeur->pays = $request->pays;
-        $distributeur->telephone = $request->telephone;
+        $distributeur->nom = $request->nom != null ? $request->nom : $distributeur->nom;
+        $distributeur->adresse = $request->adresse != null ? $request->adresse : $distributeur->adresse;
+        $distributeur->cpostal = $request->cpostal != null ? $request->cpostal : $distributeur->cpostal;
+        $distributeur->ville = $request->ville != null ? $request->ville : $distributeur->ville;
+        $distributeur->pays = $request->pays != null ? $request->pays : $distributeur->pays;
+        $distributeur->telephone = $request->telephone != null ? $request->telephone : $distributeur->telephone;
         $distributeur->save();
 
         return response()->json(
@@ -288,10 +294,10 @@ class DistributeurController extends Controller
      *     path="/distributeur/{id_distributeur}",
      *     summary="Delete a distributor",
      *     description="Use this method to delete a distributor based on its id.",
-     *     operationId="deleteDistributor",
+     *     operationId="destroyDistributeur",
      *     tags={"distributeur"},
      *     @SWG\Parameter(
-     *         description="Distributor ID to delete",
+     *         description="Distributor ID",
      *         in="path",
      *         name="id_distributeur",
      *         required=true,
@@ -304,7 +310,7 @@ class DistributeurController extends Controller
      *     ),
      *     @SWG\Response(
      *         response=404,
-     *         description="Invalid distributor value"
+     *         description="Distributor not found"
      *     )
      *
      * )
@@ -315,7 +321,7 @@ class DistributeurController extends Controller
 
         if(empty($distributeur)){
             return response()->json(
-                ['error' => 'This distributor does not exist'],
+                ['error' => 'Distributor not found'],
                 404);
         }
 
